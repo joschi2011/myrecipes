6907ced4c2ffa14f8de672957dfb44fc2a9b4ac6
Simone <sjaschko@yahoo.com>
2015-10-27 20:15:03 +0000

    Adding and removal of ActiveAdmin due to pagination conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027154259) do
+ActiveRecord::Schema.define(version: 20151027200951) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
