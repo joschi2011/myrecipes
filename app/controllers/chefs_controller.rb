34b1847d4a6bb0b41035b3bbdfa778516ac1f600
Simone <sjaschko@yahoo.com>
2015-10-27 16:07:55 +0000

    Added videos, and added a field to the chefs table to accomodate the profile

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -42,7 +42,7 @@ class ChefsController < ApplicationController
   private
   
     def chef_params
-      params.require(:chef).permit(:chefname, :email, :password)
+      params.require(:chef).permit(:chefname, :email, :password, :profile)
     end
     
     def set_chef
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027021805) do
+ActiveRecord::Schema.define(version: 20151027154259) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151027021805) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.text     "profile"
   end
 
   create_table "ingredients", force: :cascade do |t|
